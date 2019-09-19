16bfe131adbf6520804476c6d98febea00c4072c
Lidii <lidiiany92@gmail.com>
2019-09-18 19:26:24 -0700

    Added apollo client

---
+++ client/components/SongList.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class SongList extends Component {
+    render() {
+        return (
+            <div>
+                SongList
+            </div>
+        );
+
+    }
+}
+
+export default SongList;
