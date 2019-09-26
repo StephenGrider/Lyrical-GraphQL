10e2a167bc4c849bfb9ad70369113b42cb98898b
Lidii <lidiiany92@gmail.com>
2019-09-25 19:26:15 -0700

    Fetching list of songs

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,14 +1,44 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
+import {graphql} from 'react-apollo';
 
 class SongList extends Component {
+
+    renderSongs() {
+        return this.props.data.songs.map(song => {
+            return (
+                <li>
+                    {song.title}
+                </li>
+            );
+        })
+    }
+
+
     render() {
+        if (this.props.data.loading) {
+            return (
+                <div>
+                    Loading...
+                </div>
+            );
+        }
+        
         return (
             <div>
-                SongList
+                {this.renderSongs()}
             </div>
         );
 
     }
 }
 
-export default SongList;
+const query = gql`
+   {
+       songs {
+           title
+       }
+   } 
+`;
+
+export default graphql(query)(SongList);
