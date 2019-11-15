e9c85d469c312fa63def6161bbca5e9421321d8f
Lidii <lidiiany92@gmail.com>
2019-11-15 10:26:50 -0800

    LyricList component

--- client/components/SongDetail.js
+++ client/components/SongDetail.js
@@ -3,6 +3,7 @@ import {graphql} from 'react-apollo';
 import fetchSong from '../quieries/fetchSong';
 import {Link} from 'react-router-dom';
 import LyricCreate from './LyricCreate';
+import LyricList from './LyricList';
 
 export class SongDetail extends Component {
     render() {
@@ -15,6 +16,7 @@ export class SongDetail extends Component {
                 <div>
                     <Link to="/">Back</Link>
                     <h3>{song.title}</h3>
+                    <LyricList />
                     <LyricCreate songId={this.props.match.params.id}/>
                 </div>
             )
